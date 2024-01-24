5ab3b7d8efbbc8917107d7a70a2e587cedb2b226
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-01-24 16:02:53 +0800

    chore: Created the main component

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ZeroTransToAuto from './components/ZeroTransToAuto.vue'
+
+let isOpen = ref(false)
+let isOnHover = ref(true)
+let direction = ref('y')
+let isPaddingDisabled = ref(false)
+let isTransitionDisabled = ref(false)
+let isWidthFitDisabled = ref(false)
+
+function toggleDirection() {
+  direction.value = direction.value === 'y' ? 'x' : direction.value === 'x' ? 'y' : 'x'
+}
+function toggleHoverabilityzitationalismister() {
+  isOpen.value = false
+  isOnHover.value = !isOnHover.value
+}
+function togglePaddingabilityzitationalismister() {
+  isOnHover.value = false
+  isPaddingDisabled.value = !isPaddingDisabled.value
+}
+</script>
+
+<template>
+  <main>
+    <div class="testing-container">
+      <div class="header">
+        <h1>Testing Area</h1>
+        <p>Currently in <span :class="isOnHover ? 'highlight' : 'muted'">Hover</span></p>
+      </div>
+      <div class="ztta-container">
+        <ZeroTransToAuto
+          :isOpen="isOpen"
+          :transitionDirectionAxis="direction"
+          :isOnHover="isOnHover"
+          :disableDefaultPadding="isPaddingDisabled"
+          :disableDefaultTransitionAll="isTransitionDisabled"
+          :disableWidthFit="isWidthFitDisabled"
+        >
+          <span>HelloMadapaka!!</span>
+        </ZeroTransToAuto>
+      </div>
+      <div class="buttons">
+        <button :disabled="isOnHover" @click="isOpen = !isOpen">
+          {{
+            isOnHover ? 'Hover the Element inside the Blue Border' : `${isOpen ? 'Close' : 'Open'}`
+          }}
+        </button>
+        <div>
+          <button :disabled="isPaddingDisabled" @click="toggleHoverabilityzitationalismister()">
+            Toggle Hoverabilityzitationalismister
+          </button>
+          <button @click="toggleDirection()">Toggle Direction</button>
+          <button @click="togglePaddingabilityzitationalismister()">
+            Padding {{ isPaddingDisabled ? 'Disabled' : 'Enabled' }}
+          </button>
+          <button @click="isTransitionDisabled = !isTransitionDisabled">
+            Transition {{ isTransitionDisabled ? 'Disabled' : 'Enabled' }}
+          </button>
+          <button @click="isWidthFitDisabled = !isWidthFitDisabled">
+            Width {{ isWidthFitDisabled ? 'Fit' : 'Auto' }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100svh;
+  display: flex;
+}
+main > * {
+  width: 50%;
+}
+.testing-container {
+  display: grid;
+  row-gap: 1rem;
+  border: 1px solid;
+  padding: 2rem;
+  margin: auto;
+}
+.highlight {
+  color: brown;
+  font-weight: bold;
+}
+.muted {
+  color: #a9a9a9;
+}
+.buttons {
+  display: grid;
+  gap: 1rem;
+}
+.buttons > div {
+  display: flex;
+  gap: 1rem;
+}
+.header {
+  display: grid;
+  gap: 1rem;
+}
+.header > * {
+  margin: 0;
+}
+.ztta-container {
+  padding: 1rem;
+  border: 1px solid blue;
+}
+.ztta-container span {
+  color: white;
+}
+</style>
---
+++ src/components/ZeroTransToAuto.vue
@@ -0,0 +1,84 @@
+<template>
+  <div
+    class="zero-to-auto-container"
+    :class="`${computedClass} ${props.customClass}`"
+    :style="`
+      ${props.disableDefaultBgColor ? '' : 'background-color: gray;'} ${props.disableDefaultPadding ? '' : 'padding: 10px;'}
+    `"
+  >
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+export interface Props {
+  customClass?: string
+  isOpen?: boolean
+  transitionDirectionAxis?: string
+  isOnHover?: boolean
+  disableDefaultBgColor?: boolean
+  disableDefaultPadding?: boolean
+  disableDefaultTransitionAll?: boolean
+  disableWidthFit?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isOpen: false,
+  transitionDirectionAxis: 'y',
+  isOnHover: true,
+  disableDefaultBgColor: false,
+  disableDefaultPadding: false,
+  disableDefaultTransitionAll: false,
+  disableWidthFit: false
+})
+
+const computedClass = computed(() => {
+  let gridTemplate = props.transitionDirectionAxis === 'x' ? 'col' : 'row'
+  let isOpenComputed = props.isOpen ? `grid-${gridTemplate}-1` : `grid-${gridTemplate}-0`
+  let isOnHoverComputed = props.isOnHover ? `open-${gridTemplate}-on-hover` : ''
+  let isOnTransition = props.disableDefaultTransitionAll ? '' : 'transition-all'
+  let isWidthFit = props.disableWidthFit ? '' : 'w-fit'
+  return `${isOpenComputed} ${isOnHoverComputed} ${isOnTransition} ${isWidthFit}`
+})
+</script>
+
+<style scoped>
+.zero-to-auto-container {
+  display: grid;
+}
+.transition-all {
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+}
+
+.w-fit {
+  width: fit-content;
+}
+
+.zero-to-auto-container > div {
+  overflow: clip;
+}
+.open-row-on-hover:hover {
+  grid-template-rows: 1fr;
+}
+.open-col-on-hover:hover {
+  grid-template-columns: 1fr;
+}
+
+.grid-row-0 {
+  grid-template-rows: 0fr;
+}
+.grid-row-1 {
+  grid-template-rows: 1fr;
+}
+.grid-col-0 {
+  grid-template-columns: 0fr;
+}
+.grid-col-1 {
+  grid-template-columns: 1fr;
+}
+</style>
